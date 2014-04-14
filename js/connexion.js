67d3ec108b30f5f9e249e30467b4982aa7fc8d5d
Guilhem Albert <guilhem.albert@insa-lyon.fr>
2014-04-14 16:44:50 +0200

    test

--- js/connexion.js
+++ js/connexion.js
@@ -36,6 +36,7 @@ $(document).on("pageinit", "#connexion", function() {
 	});
 	
 	socket.on('resultConnexionUtilisateur', function(data) {
+		//navigator.splashscreen.hide();
 		if (data.connexionAccordee) {
 			$.mobile.changePage("#Accueil_jeu");
 			idJoueur = data.idJoueur;
