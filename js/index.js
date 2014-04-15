eb1d50aeafffd9c6bd77d85cd641c1655af63397
Guilhem Albert <guilhem.albert@insa-lyon.fr>
2014-04-15 15:57:54 +0200

    test notif

--- js/index.js
+++ js/index.js
@@ -1,16 +1,38 @@
 // JavaScript Document
-var pushNotification = window.plugins.pushNotification;
+var app = {
+// Application Constructor
+initialize: function() {
+    this.bindEvents();
+},
+// Bind Event Listeners
+//
+// Bind any events that are required on startup. Common events are:
+// 'load', 'deviceready', 'offline', and 'online'.
+bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+},
+// deviceready Event Handler
+//
+// The scope of 'this' is the event. In order to call the 'receivedEvent'
+// function, we must explicity call 'app.receivedEvent(...);'
+onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+    myApp.start(); //this is where I put the call to my App's functionality relying on device APIs
+},
+// Update DOM on a Received Event
+receivedEvent: function(id) { // I didn't really use this, yet I left it in here as it's in the demo
+   var pushNotification = window.plugins.pushNotification;
 pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"494854872923","ecb":"app.onNotificationGCM"});
 // result contains any message sent from the plugin call
-successHandler= function(result) {
+successHandler: function(result) {
     alert('Callback Success! Result = '+result)
 };
 
-errorHandler=function(error) {
+errorHandler:function(error) {
     alert(error);
 };
 
-onNotificationGCM= function(e) {
+onNotificationGCM: function(e) {
         switch( e.event )
         {
             case 'registered':
@@ -35,4 +57,5 @@ onNotificationGCM= function(e) {
               break;
         }
     }
-	
+}
+};
